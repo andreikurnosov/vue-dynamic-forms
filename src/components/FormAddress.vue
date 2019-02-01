f4e92c4e7421977e896fe9e0559cb6e07ed0f6d0
nixdie <nixdie@yandex.ru>
2019-02-01 12:17:00 +0700

    preserve component state

--- src/components/FormAddress.vue
+++ src/components/FormAddress.vue
@@ -52,12 +52,14 @@
     },
     methods: {
       submit () {
-        if (!this.$v.$invalid) {
-          this.$emit('update', {
+        this.$emit('update', {
+          data: {
             recipient: this.form.recipient,
             address: this.form.address
-          })
-        }
+          },
+
+          valid: !this.$v.$invalid
+        })
       }
     }
   }
