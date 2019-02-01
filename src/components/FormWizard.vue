0808bbf8144c39e4fa8e8c716c39344edc646cc5
nixdie <nixdie@yandex.ru>
2019-02-01 12:53:16 +0700

    submit form & fake api call

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -1,38 +1,48 @@
 <template>
   <div>
-    <keep-alive>
-      <component
-        ref="currentUser"
-        :is="currentStep"
-        @update="processStep"
-        :wizzard-data="form"
-      ></component>
-    </keep-alive>
+    <div v-if="wizzardInProgress">
+      <keep-alive>
+        <component
+          ref="currentUser"
+          :is="currentStep"
+          @update="processStep"
+          :wizzard-data="form"
+        ></component>
+      </keep-alive>
 
-    <div class="progress-bar">
-      <div :style="`width: ${progress}%;`"></div>
+      <div class="progress-bar">
+        <div :style="`width: ${progress}%;`"></div>
+      </div>
+
+      <!-- Actions -->
+      <div class="buttons">
+        <button
+          @click="goBack"
+          v-if="currentStepNumber > 1"
+          class="btn-outlined"
+        >Back
+        </button>
+        <button
+          @click="nextButtonAction"
+          :disabled="!canGoNext"
+          class="btn"
+        >{{isLastStep ? 'Complete Order' : 'Next'}}</button>
+      </div>
+
+      <pre><code>{{form}}</code></pre>
     </div>
-
-    <!-- Actions -->
-    <div class="buttons">
-      <button
-        @click="goBack"
-        v-if="currentStepNumber > 1"
-        class="btn-outlined"
-      >Back
-      </button>
-      <button
-        @click="goNext"
-        :disabled="!canGoNext"
-        class="btn"
-      >Next</button>
+    <div v-else>
+      <h1 class="title">Thank you!</h1>
+      <h2 class="subtitle">We look forward to shipping your first box!</h2>
+      <p class="text-center">
+        <a href="#0" class="btn" target="_blank">Go somewhere cool!</a>
+      </p>
     </div>
-
-    <pre><code>{{form}}</code></pre>
   </div>
 </template>
 
 <script>
+import {postFormToDB} from '../api'
 import FormPlanPicker from './FormPlanPicker'
 import FormUserDetails from './FormUserDetails'
 import FormAddress from './FormAddress'
@@ -68,6 +78,17 @@ export default {
     }
   },
   computed: {
+
+    isLastStep () {
+      return this.currentStepNumber === this.length
+
+    },
+
+    wizzardInProgress () {
+      return this.currentStepNumber <= this.length
+
+    },
+
     length () {
       return this.steps.length
     },
@@ -81,6 +102,22 @@ export default {
     }
   },
   methods: {
+    submitOrder () {
+      postFormToDB(this.form)
+        .then(() => {
+          console.log('form is submitted', this.form)
+          this.currentStepNumber++
+        })
+    },
+
+    nextButtonAction () {
+      if (this.isLastStep) {
+        this.submitOrder()
+      } else {
+        this.goNext()
+      }
+    },
+
     processStep (step) {
       Object.assign(this.form, step.data)
       this.canGoNext = step.valid
